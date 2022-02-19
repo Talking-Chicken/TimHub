54d0e88a98e371d81a5a4dcb2e8a9d925ef95c06
Leo Ji <sj3026@nyu.edu>
2022-02-18 22:28:02 -0500

    updating yarn spinner to 2.1.0

--- Assets/Scripts/State/PlayerStateDialogue.cs
+++ Assets/Scripts/State/PlayerStateDialogue.cs
@@ -11,6 +11,8 @@ public class PlayerStateDialogue : PlayerStateBase
     public override void updateState(PlayerControl player) {
         if (!player.runner.IsDialogueRunning)
             player.changeState(player.stateExplore);
+        if (Input.GetMouseButtonUp(0))
+            player.runner.GetComponentInChildren<LineView>().OnContinueClicked();
     }
 
     public override void fixedUpdate(PlayerControl player)
