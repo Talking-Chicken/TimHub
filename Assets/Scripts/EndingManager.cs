9019ae64a9e0e8c85e25677968bc01648286c2ab
Leo Ji <sj3026@nyu.edu>
2022-05-07 23:11:04 -0400

    report answer functions should work now

--- Assets/Scripts/EndingManager.cs
+++ Assets/Scripts/EndingManager.cs
@@ -7,17 +7,33 @@ using NaughtyAttributes;
 
 public class EndingManager : MonoBehaviour
 {
-    [SerializeField, Foldout("player attempt answer")] TextMeshProUGUI criminalAnswerText, causeAnswerText, timeAnswerText, locationAnswerText, detailAnswerText; 
+    [SerializeField, Foldout("player attempt answer")] TMP_Dropdown criminalAnswerText, causeAnswerText, timeAnswerText, locationAnswerText, detailAnswerText; 
     [SerializeField, BoxGroup("THE correct answer"),ResizableTextArea]string criminal, cause, time, location, detail;
+    public static int answerCorrectness = 0;
 
     [YarnFunction("Report_Answer")]
-    public int reportAnswer() {
+    public static int reportAnswer() {
+        return answerCorrectness;
+    }
+
+    public void checkAnswer() {
         int percentCorrect = 0;
-        if (criminalAnswerText.text.ToLower().Trim().Equals(criminal.ToLower().Trim())) percentCorrect += 20;
-        if (causeAnswerText.text.ToLower().Trim().Equals(cause.ToLower().Trim())) percentCorrect += 20;
-        if (timeAnswerText.text.ToLower().Trim().Equals(time.ToLower().Trim())) percentCorrect += 20;
-        if (locationAnswerText.text.ToLower().Trim().Equals(location.ToLower().Trim())) percentCorrect += 20;
-        if (detailAnswerText.text.ToLower().Trim().Equals(detail.ToLower().Trim())) percentCorrect += 20;
-        return percentCorrect;
+
+        if (criminalAnswerText.value > 0)
+            if (criminalAnswerText.options[criminalAnswerText.value].text.ToLower().Trim().Equals(criminal.ToLower().Trim())) percentCorrect += 20;
+
+        if (causeAnswerText.value > 0)
+            if (causeAnswerText.options[causeAnswerText.value].text.ToLower().Trim().Equals(cause.ToLower().Trim())) percentCorrect += 20;
+
+        if (timeAnswerText.value > 0)
+            if (timeAnswerText.options[timeAnswerText.value].text.ToLower().Trim().Equals(time.ToLower().Trim())) percentCorrect += 20;
+
+        if (locationAnswerText.value > 0)
+            if (locationAnswerText.options[locationAnswerText.value].text.ToLower().Trim().Equals(location.ToLower().Trim())) percentCorrect += 20;
+
+        if (detailAnswerText.value > 0)
+            if (detailAnswerText.options[detailAnswerText.value].text.ToLower().Trim().Equals(detail.ToLower().Trim())) percentCorrect += 20;
+        answerCorrectness = percentCorrect;
+        Debug.Log(answerCorrectness + " is the correctness");
     }
 }
