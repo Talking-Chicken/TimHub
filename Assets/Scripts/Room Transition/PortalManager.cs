90db984bf90bd654209178155ae8d9cf9e7fcd79
Max Seavey <58485607+cvmaxie@users.noreply.github.com>
2022-05-08 01:44:33 -0400

    transition function and nero appearing function
    implemented in will yarn

--- Assets/Scripts/Room Transition/PortalManager.cs
+++ Assets/Scripts/Room Transition/PortalManager.cs
@@ -12,31 +12,42 @@ public class PortalManager : MonoBehaviour
         portals.AddRange(FindObjectsOfType<Portal>());
     }
 
-    
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
             StartCoroutine(transitionAnimation());
     }
 
-    public static void teleport() {
-        if (currentPortal != null) {
+    public static void teleport()
+    {
+        if (currentPortal != null)
+        {
             for (int i = 0; i < portals.Count; i++)
             {
-                if (currentPortal.equalsButNotSelf(portals[i])) {
+                if (currentPortal.equalsButNotSelf(portals[i]))
+                {
                     FindObjectOfType<PlayerControl>().teleport(portals[i].transform.position);
                 }
             }
         }
-        
+
+    }
+
+    [YarnCommand("Transition_Anim")]
+    public void transitionAnim()
+    {
+        StartCoroutine(transitionAnimation());
     }
 
     [YarnCommand("Room_Change")]
-    public void roomChange() {
+    public void roomChange()
+    {
         StartCoroutine(transitionAnimation());
-    } 
+    }
 
-    IEnumerator transitionAnimation() {
+    IEnumerator transitionAnimation()
+    {
         TransitionManager.Instance.roomTransitionAnimation();
         yield return new WaitForSeconds(1.0f);
         teleport();
